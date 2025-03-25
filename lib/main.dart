1bfbbfb92fd02acc1bf2997612c04f1b31e67eec
LaksenK <laksenkalmitha11@gmail.com>
2025-03-26 00:50:45 +0530

    sprint 1 third commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/':(context)=>Home(),
+        '/':(context)=>Login(),
       },
  );
   }
